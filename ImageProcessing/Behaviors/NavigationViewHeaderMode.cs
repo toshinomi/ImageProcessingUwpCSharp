b80050c2e86eb880971ff7411c99f7a76d98c9ce
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-09-02 22:20:17 +0900

    プロジェクト ファイルを追加します。

---
+++ ImageProcessing/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageProcessing.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
---
+++ ImageProcessing/Views/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ImageProcessing.Views
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public async void OnClickBtnFileSelect(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
+            picker.FileTypeFilter.Add("*");
+            picker.FileTypeFilter.Add(".png");
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+
+            var file = await picker.PickSingleFileAsync();
+            if (file == null)
+            {
+                return;
+            }
+
+            var openFile = await file.OpenReadAsync();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.SetSource(openFile);
+            this.pictureBoxOriginal.Source = bitmap;
+        }
+    }
+}
